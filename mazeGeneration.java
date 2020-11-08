43a8123bc1754f6b3d9cbb913fa3545675e1855a
salazarbrandon1257 <salazarbrandon1257@gmail.com>
2020-11-08 17:13:57 -0600

    added DisjSets class

--- mazeGeneration.java
+++ mazeGeneration.java
@@ -1,7 +1,54 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class mazeGeneration{
 
+    ArrayList<Integer> arrlist = new ArrayList<Integer>(5);
+
+
+    static class DisjSets
+{
+        /** Construct the disjoint sets object.
+         * @param numElements the initial number of disjoint sets.
+         */
+        public DisjSets(int numElements)
+        {
+            s = new int [ numElements ];
+            for( int i = 0; i < s.length; i++ )
+                s[ i ] = -1;
+        }
+
+        /**   Union two disjoint sets.  
+         *     Assume root1 and root2 are distinct and represent set names.
+         * @param root1 the root of set 1.
+         * @param root2 the root of set 2.          */
+        public void union( int root1, int root2 )
+        {
+            s[ root2 ] = root1;
+        }
+        
+        /** Perform a find.   Error checks omitted again for simplicity.
+         * @param x the element being searched for.
+         * @return the set containing x.         */
+        public int find( int x )
+        {
+            if( s[ x ] < 0 )
+                return x;
+            else
+                return find( s[ x ] );
+        }
+
+        private int [ ] s;
+}
+
+
+
     public static void main(String[] args){ 
+        DisjSets sets = new DisjSets(10);
+        System.out.println(sets.find(1));
+        sets.union(0, 1);
+        System.out.println(sets.find(1));
+        
+
     } 
 }
